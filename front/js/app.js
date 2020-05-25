02b0d67f268590d1a33cfa13a2d94493ab7507ab
unknown <kirtanushah@gmail.com>
2020-05-25 00:03:44 -0700

    Add whiteboard

--- front/js/app.js
+++ front/js/app.js
@@ -3,6 +3,7 @@ require('/lib/velocity.min.js')
 
 import landing from '/pages/landing.html'
 import dashboard from '/pages/dashboard.html'
+import whiteboard from '/pages/whiteboard.html'
 import liveQuiz from '/pages/live-quiz.html'
 import smartFeedback from '/pages/smart-feedback.html'
 import feedbackAnalysis from '/pages/feedback-analysis.html'
@@ -17,35 +18,43 @@ $(document).ready(function() {
     $('#dash-button').click(() => {
         switchTo(dashboard, 'down')
 
-        $('#new-live-quiz').click(function() {
-            switchTo(liveQuiz, false)
-            let quiz = new Quiz(window.globalSocket)
-            quiz.initEvents()
-        })
-        $('#new-smart-feedback').click(function() {
-            switchTo(smartFeedback, false)
-            $('#send-feedback-form').click(function() {
-                console.log("clicked")
-                let q = $('#feedback-question-input').val()
-                window.globalSocket.emit('smartFeedback', q, new Date())
-            })
-        })
-        $('#feedback-info').click(function() {
-            switchTo(feedbackAnalysis, false)
-            socket.emit('requestFeedbacks')
-            socket.once('feedbacks', feedbacks => {
-                let c = '<div id="constructive">Constructive</div>'
-                let d = '<div id="destructive">Destructive</div>'
-                for(let fs of Object.values(feedbacks)) {
-                    for(let f of fs) {
-                        if(f.analysis.constructive) 
-                            c += f.text
-                        else
-                            d += f.text
-                    }
-                }
-                $('#smart-info').html(c + d)
-            })
-        })
+        $('#new-white-board').click(newWhiteboard)
+        $('#new-live-quiz').click(newLiveQuiz)
+        $('#new-smart-feedback').click(newSmartFeedback)
+        $('#feedback-info').click(onFeedbackInfo)
     })
 })
+
+function newWhiteboard() {
+    switchTo(whiteboard, false)
+}
+function newLiveQuiz() {
+    switchTo(liveQuiz, false)
+    let quiz = new Quiz(window.globalSocket)
+    quiz.initEvents()
+}
+function newSmartFeedback() {
+    switchTo(smartFeedback, false)
+    $('#send-feedback-form').click(function() {
+        console.log("clicked")
+        let q = $('#feedback-question-input').val()
+        window.globalSocket.emit('smartFeedback', q, new Date())
+    })
+}
+function onFeedbackInfo() {
+    switchTo(feedbackAnalysis, false)
+    window.globalSocket.emit('requestFeedbacks')
+    window.globalSocket.once('feedbacks', feedbacks => {
+        let c = '<div id="constructive">Constructive</div>'
+        let d = '<div id="destructive">Destructive</div>'
+        for(let fs of Object.values(feedbacks)) {
+            for(let f of fs) {
+                if(f.analysis.constructive) 
+                    c += `<div>${f.text}</div>`
+                else
+                    d += `<div>${f.text}</div>`
+            }
+        }
+        $('#smart-info').html(c + d)
+    })
+}
