6e61e6195624396b36d18ca7247037e1cabfe046
unknown <kirtanushah@gmail.com>
2020-05-24 23:24:49 -0700

    Integerate analysis page

--- front/js/app.js
+++ front/js/app.js
@@ -5,6 +5,7 @@ import landing from '/pages/landing.html'
 import dashboard from '/pages/dashboard.html'
 import liveQuiz from '/pages/live-quiz.html'
 import smartFeedback from '/pages/smart-feedback.html'
+import feedbackAnalysis from '/pages/feedback-analysis.html'
 import Quiz from './Quiz'
 import { switchTo } from './switch.js'
 
@@ -27,6 +28,23 @@ $(document).ready(function() {
                 console.log("clicked")
                 let q = $('#feedback-question-input').val()
                 window.globalSocket.emit('smartFeedback', q, new Date())
+            })
+        })
+        $('#feedback-info').click(function() {
+            switchTo(feedbackAnalysis, false)
+            socket.emit('requestFeedbacks')
+            socket.once('feedbacks', feedbacks => {
+                let c = '<div id="constructive">Constructive</div>'
+                let d = '<div id="destructive">Destructive</div>'
+                for(let fs of Object.values(feedbacks)) {
+                    for(let f of fs) {
+                        if(f.analysis.constructive) 
+                            c += f.text
+                        else
+                            d += f.text
+                    }
+                }
+                $('#smart-info').html(c + d)
             })
         })
     })
